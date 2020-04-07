a2681e3e5c26ab528bf380d2e38b05abfa3b4ea5
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-07 12:25:35 -0700

    Add files via upload

---
+++ timer1.js
@@ -0,0 +1,19 @@
+let beep = process.stdout.write("\x07");
+let myArgs = process.argv.slice(2);
+
+const timer = function(myArgs) {
+  for (let el of myArgs) {
+    if (el === "true" || el === "false") {
+      return;
+    } else if (el > 0) {
+      setTimeout(() => {
+        console.log("beep");
+        beep;
+      }, Number(el) * 1000);
+    } else {
+      return;
+    }
+  }
+};
+
+timer([3, 5, 9, 10, 15]);
